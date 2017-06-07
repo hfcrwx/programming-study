ca36a297ad7dc67ee19b575a54a0e5af694ee9a6
hfcrwx <hfcrwx@163.com>
2017-06-08 07:54:09 +0800

    update 11process

--- sp/11process/01fork.c
+++ sp/11process/01fork.c
@@ -1,26 +1,18 @@
 #include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
 
 #include <stdlib.h>
 #include <stdio.h>
-#include <errno.h>
-#include <string.h>
 #include <signal.h>
 
-
 #define ERR_EXIT(m) \
-	do \
-	{ \
+	do { \
 		perror(m); \
 		exit(EXIT_FAILURE); \
-	} while(0)
+	} while (0)
 
 int main(int argc, char *argv[])
 {
-	signal(SIGCHLD, SIG_IGN);
+	signal(SIGCHLD, SIG_IGN); // 避免僵尸进程
 	printf("before fork pid = %d\n", getpid());
 	pid_t pid;
 	pid = fork();
@@ -30,11 +22,12 @@ int main(int argc, char *argv[])
 	if (pid > 0)
 	{
 		printf("this is parent pid=%d childpid=%d\n", getpid(), pid);
-		sleep(100);
+		sleep(100); // 子进程成为僵尸进程
 	}
 	else if (pid == 0)
 	{
 		printf("this is child pid=%d parentpid=%d\n", getpid(), getppid());
+//		sleep(100); // 成为孤儿进程
 	}
 	return 0;
 }
