a757ce3e0cb8745e4f667126c58c9b6197c72387
hfcrwx <hfcrwx@163.com>
2017-06-18 10:21:33 +0800

    update 22signal

--- sp/22signal/01setitimer.c
+++ sp/22signal/01setitimer.c
@@ -1,36 +1,28 @@
-#include <unistd.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#include <fcntl.h>
-
-#include <stdlib.h>
-#include <stdio.h>
-#include <errno.h>
-#include <string.h>
 #include <signal.h>
 #include <sys/time.h>
+#include <unistd.h>
 
+#include <stdio.h>
+#include <stdlib.h>
 
 #define ERR_EXIT(m) \
-	do \
-	{ \
+	do { \
 		perror(m); \
 		exit(EXIT_FAILURE); \
-	} while(0)
+	} while (0)
 
 void handler(int sig)
 {
 	printf("recv a sig=%d\n", sig);
 }
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
 	if (signal(SIGALRM, handler) == SIG_ERR)
 		ERR_EXIT("signal error");
 
-	struct timeval tv_interval = {1, 0};
-	struct timeval tv_value = {5, 0};
+	struct timeval tv_interval = { 1, 0 };
+	struct timeval tv_value = { 5, 0 };
 	struct itimerval it;
 	it.it_interval = tv_interval;
 	it.it_value = tv_value;
@@ -38,5 +30,6 @@ int main(int argc, char *argv[])
 
 	for (;;)
 		pause();
+
 	return 0;
 }
