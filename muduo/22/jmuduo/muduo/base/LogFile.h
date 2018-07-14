079570d93ca168b4a649a3f421464d7212fd8ed6
hfcrwx <hfcrwx@163.com>
2018-07-14 12:10:44 +0800

    update 22

--- muduo/22/jmuduo/muduo/base/LogFile.h
+++ muduo/22/jmuduo/muduo/base/LogFile.h
@@ -30,9 +30,9 @@ class LogFile : boost::noncopyable
 
   const string basename_;		// 日志文件basename
   const size_t rollSize_;		// 日志文件达到rolSize_换一个新文件
-  const int flushInterval_;		// 日志写入间隔时间
+  const int flushInterval_;		// 日志写入间隔时间，不是每次写日志都会写入文件
 
-  int count_;
+  int count_; // 初始值是0，当达到kCheckTimeRoll_的时候检测是否换一个新的日志文件，或者是否需要将日志写入文件中
 
   boost::scoped_ptr<MutexLock> mutex_;
   time_t startOfPeriod_;	// 开始记录日志时间（调整至零点的时间）
