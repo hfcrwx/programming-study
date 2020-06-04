6b56c45baf72a2e17f82f81c1b9bbd89d43719f8
hfcrwx <hfcrwx@163.com>
2020-06-04 12:09:31 +0800

    add 18_01_DeleteNodeInList

---
+++ CodingInterviewChinese2/18_01_DeleteNodeInList/main.cc
@@ -0,0 +1,36 @@
+// 面试题18（一）：在O(1)时间删除链表结点
+// 题目：给定单向链表的头指针和一个结点指针，定义一个函数在O(1)时间删除该
+// 结点。
+
+#include <base/List.h>
+
+void DeleteNode(ListNode** head, ListNode* target) {
+  if (head==nullptr || *head==nullptr || target==nullptr) {
+    return;
+  }
+  if (target->next_!=nullptr) {
+    ListNode* next = target->next_;
+    target->value_ = next->value_;
+    target->next_ = next->next_;
+    delete next;
+    next = nullptr;
+  } else {
+    if (target == *head) {
+      delete target;
+      target = nullptr;
+      *head = nullptr;
+    } else {
+      ListNode* node = *head;
+      while (node->next_ != target) {
+        node = node->next_;
+      }
+      node->next_ = nullptr;
+      delete target;
+      target = nullptr;
+    }
+  }
+}
+
+int main() {
+  return 0;
+}
