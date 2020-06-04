447e8b8e845553bcdadb9771a57e87a094a4c415
hfcrwx <hfcrwx@163.com>
2020-06-04 19:05:58 +0800

    add 22_KthNodeFromEnd

---
+++ CodingInterviewChinese2/22_KthNodeFromEnd/main.cc
@@ -0,0 +1,31 @@
+// 面试题22：链表中倒数第k个结点
+// 题目：输入一个链表，输出该链表中倒数第k个结点。为了符合大多数人的习惯，
+// 本题从1开始计数，即链表的尾结点是倒数第1个结点。例如一个链表有6个结点，
+// 从头结点开始它们的值依次是1、2、3、4、5、6。这个链表的倒数第3个结点是
+// 值为4的结点。
+
+#include <base/List.h>
+#include <stdint.h>
+
+const ListNode* findKthToTail(const ListNode* head, uint32_t k) {
+  if (head==nullptr || k==0) {
+    return nullptr;
+  }
+  const ListNode* ahead = head;
+  for (uint32_t i = 0; i < k-1; ++i) {
+    ahead = ahead->next_;
+    if (ahead==nullptr) {
+      return nullptr;
+    }
+  }
+  const ListNode* behind = head;
+  while (ahead->next_ != nullptr) {
+    ahead = ahead->next_;
+    behind = behind->next_;
+  }
+  return behind;
+}
+
+int main() {
+  return 0;
+}
