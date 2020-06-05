d63753fce9dbad725bbe5e260555c7d24a0dc637
hfcrwx <hfcrwx@163.com>
2020-06-05 16:52:42 +0800

    add 26_SubstructureInTree

---
+++ CodingInterviewChinese2/26_SubstructureInTree/main.cc
@@ -0,0 +1,46 @@
+// 面试题26：树的子结构
+// 题目：输入两棵二叉树A和B，判断B是不是A的子结构。
+
+//                  8                8
+//              /       \           / \
+//             8         7         9   2
+//           /   \
+//          9     2
+//               / \
+//              4   7
+
+#include <base/BinaryTree.h>
+
+bool doesTree1HasTree2(BinaryTreeNode* root1, BinaryTreeNode* root2);
+
+bool hasSubTree(BinaryTreeNode* root1, BinaryTreeNode* root2) {
+  bool result = false;
+  if (root1!=nullptr && root2!=nullptr) {
+    if (root1->value_==root2->value_) {
+      result = doesTree1HasTree2(root1, root2);
+    }
+    if (!result) {
+      result = hasSubTree(root1->left_, root2);
+    }
+    if (!result) {
+      result = hasSubTree(root1->right_, root2);
+    }
+  }
+  return result;
+}
+
+bool doesTree1HasTree2(BinaryTreeNode* root1, BinaryTreeNode* root2) {
+  if (root2==nullptr) {
+    return true;
+  }
+  if (root1==nullptr) {
+    return false;
+  }
+  return root1->value_==root2->value_
+      && doesTree1HasTree2(root1->left_, root2->left_)
+      && doesTree1HasTree2(root1->right_, root2->right_);
+}
+
+int main() {
+  return 0;
+}
