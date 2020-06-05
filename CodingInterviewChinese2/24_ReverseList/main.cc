50bca19949fd5ea126c8a9c9964cbc21d8589255
hfcrwx <hfcrwx@163.com>
2020-06-05 10:46:58 +0800

    add 24_ReverseList

---
+++ CodingInterviewChinese2/24_ReverseList/main.cc
@@ -0,0 +1,34 @@
+// 面试题24：反转链表
+// 题目：定义一个函数，输入一个链表的头结点，反转该链表并输出反转后链表的
+// 头结点。
+
+// ->h->i->j->k->
+// ...
+// <-h  i->j->k->
+// <-h  i  j->k->
+// <-h<-i  j->k->
+// ...
+
+#include <base/List.h>
+
+ListNode* reverseList(ListNode* head) {
+  ListNode* reverseHead = nullptr;
+
+  ListNode* pre = nullptr;
+  ListNode* cur = head;
+  while (cur!=nullptr) {
+    ListNode* next = cur->next_;
+    if (next==nullptr) {
+      reverseHead = cur;
+    }
+    cur->next_ = pre;
+
+    pre = cur;
+    cur = next;
+  }
+  return reverseHead;
+}
+
+int main() {
+  return 0;
+}
