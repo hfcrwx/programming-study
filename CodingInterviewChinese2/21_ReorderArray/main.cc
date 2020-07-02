850d140fdb75d2f57e19d17879af7ec64860802b
hfcrwx <hfcrwx@163.com>
2020-07-02 18:48:17 +0800

    update

--- CodingInterviewChinese2/21_ReorderArray/main.cc
+++ CodingInterviewChinese2/21_ReorderArray/main.cc
@@ -1,28 +1,37 @@
 // 面试题21：调整数组顺序使奇数位于偶数前面
-// 题目：输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有
-// 奇数位于数组的前半部分，所有偶数位于数组的后半部分。
+// 题目：输入一个整数数组，实现一个函数来调整该数组中数字的顺序，使得所有奇数位于数组的前半部分，所
+// 有偶数位于数组的后半部分。
 
-void reorderArray(int* arr, int len) {
-  if (arr==nullptr && len <= 0) {
+#include <stddef.h>
+
+void exchange(int* nums, size_t len) {
+  if (nums == nullptr || len == 0) {
     return;
   }
-  int* begin = arr;
-  int* end = arr + len - 1;
-  while (begin < end) {
-    while (begin < end && (*begin & 1)==1) {
-      ++begin;
+
+  size_t left = 0;
+  size_t right = len - 1;
+  while (left < right) {
+    if ((nums[left] & 1) == 1) {
+      ++left;
+      continue;
     }
-    while (begin < end && (*end & 1)==0) {
-      --end;
+    if ((nums[right] & 1) == 0) {
+      --right;
+      continue;
     }
-    if (begin < end) {
-      int tmp = *begin;
-      *begin = *end;
-      *end = tmp;
-    }
+
+    int tmp = nums[left];
+    nums[left] = nums[right];
+    nums[right] = tmp;
+
+    ++left;
+    --right;
   }
 }
 
 int main() {
+  size_t x = 0;
+  --x;
   return 0;
 }
