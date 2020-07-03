45c9002d6273168dc4ee5816d69f6cd5f5f0c449
hfcrwx <hfcrwx@163.com>
2020-07-03 13:00:02 +0800

    update

--- CodingInterviewChinese2/45_minNumber/main.cc
+++ CodingInterviewChinese2/45_minNumber/main.cc
@@ -1,19 +1,19 @@
 // 面试题45：把数组排成最小的数
-// 题目：输入一个正整数数组，把数组里所有数字拼接起来排成一个数，打印能拼
-// 接出的所有数字中最小的一个。例如输入数组{3, 32, 321}，则打印出这3个数
-// 字能排成的最小数字321323。
+// 题目：输入一个正整数数组，把数组里所有数字拼接起来排成一个数，打印能拼接出的所有数字中最小的一个
+// 。例如，输入数组{3, 32, 321}，则打印出这3个数字能排成的最小数字321323。
 
 #include <algorithm>
 #include <string>
 #include <vector>
 
-std::string minNumber(const int* numbers, size_t len) {
-  if (numbers == nullptr || len == 0) {
-    return std::string();
-  }
+#include <assert.h>
+
+std::string minNumber(const int* nums, size_t size) {
+  assert(nums != nullptr && size != 0);
+
   std::vector<std::string> strs;
-  for (size_t i = 0; i < len; ++i) {
-    strs.push_back(std::to_string(numbers[i]));
+  for (size_t i = 0; i < size; ++i) {
+    strs.push_back(std::to_string(nums[i]));
   }
   std::sort(strs.begin(),
             strs.end(),
