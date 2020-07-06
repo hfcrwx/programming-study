0ee9226999db88b0f957f8d1f19a78e33895dc8e
hfcrwx <hfcrwx@163.com>
2020-07-06 16:20:58 +0800

    update

--- CodingInterviewChinese2/09_QueueWithTwoStacks/main.cc
+++ CodingInterviewChinese2/09_QueueWithTwoStacks/main.cc
@@ -1,6 +1,6 @@
 // 面试题9：用两个栈实现队列
-// 题目：用两个栈实现一个队列。队列的声明如下，请实现它的两个函数appendTail
-// 和deleteHead，分别完成在队列尾部插入结点和在队列头部删除结点的功能。
+// 题目：用两个栈实现一个队列。队列的声明如下，请实现它的两个函数appendTail和deleteHead，
+// 分别完成在队列尾部插入结点和在队列头部删除结点的功能。
 
 #include <stack>
 
@@ -16,27 +16,32 @@ class Queue {
   std::stack<T> stack1_;
   std::stack<T> stack2_;
 };
+
 template<typename T>
 Queue<T>::Queue() {
 
 }
+
 template<typename T>
 Queue<T>::~Queue() {
 
 }
+
 template<typename T>
 void Queue<T>::appendTail(const T& v) {
   stack1_.push(v);
 }
+
 template<typename T>
 T Queue<T>::deleteHead() {
   if (stack2_.empty()) {
     while (!stack1_.empty()) {
-      T& v = stack1_.top();
+      const T& v = stack1_.top();
       stack2_.push(v);
       stack1_.pop();
     }
   }
+
   if (!stack2_.empty()) {
     T v=  stack2_.top();
     stack2_.pop();
