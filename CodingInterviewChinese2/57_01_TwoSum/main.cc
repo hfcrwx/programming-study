3c9b0ab2203d0578974c6c66ecc12e317be46ae8
hfcrwx <hfcrwx@163.com>
2020-07-10 17:00:00 +0800

    update

---
+++ CodingInterviewChinese2/57_01_TwoSum/main.cc
@@ -0,0 +1,32 @@
+// 面试题57（一）：和为s的两个数字
+// 题目：输入一个递增排序的数组和一个数字s，在数组中查找两个数，使得它们的和正好是s。
+// 如果有多对数字的和等于s，输出任意一对即可。
+
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> twoSum(std::vector<int>& nums, int target) {
+    std::vector<int> result;
+    std::vector<int>::size_type left = 0;
+    std::vector<int>::size_type right = nums.size() - 1;
+    while (left < right) {
+      int sum = nums[left] + nums[right];
+      if (sum == target) {
+        result.push_back(nums[left]);
+        result.push_back(nums[right]);
+        break;
+      } else if (sum < target) {
+        ++left;
+      } else {
+        --right;
+      }
+    }
+
+    return result;
+  }
+};
+
+int main() {
+  return 0;
+}
---
+++ CodingInterviewChinese2/57_02_FindContinuousSequence/main.cc
@@ -0,0 +1,34 @@
+// 面试题57（二）：和为s的连续正数序列
+// 题目：输入一个正数s，打印出所有和为s的连续正数序列（至少含有两个数）。
+// 例如，输入15，由于1+2+3+4+5=4+5+6=7+8=15，所以结果打印出3个连续序列1～5、
+// 4～6和7～8。
+
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<std::vector<int>> findContinuousSequence(int target) {
+    std::vector<std::vector<int>> result;
+    for (int left = 1, right = 2; left < right;) {
+      int sum = (right - left + 1)*(right + left)/2;
+      if (sum == target) {
+        std::vector<int> v;
+        for (int i = left; i <= right; ++i) {
+          v.push_back(i);
+        }
+        result.push_back(v);
+        ++left;
+      } else if (sum < target) {
+        ++right;
+      } else {
+        ++left;
+      }
+    }
+
+    return result;
+  }
+};
+
+int main() {
+  return 0;
+}
